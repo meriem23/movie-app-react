221ba5a84bde87be084c1a9344e90d1fcd7ecd7a
meriem23 <esra.msallem@gmail.com>
2020-03-11 19:22:19 +0100

    Updates

---
+++ precache-manifest.0c81df01e85ea2fcf3d24364412eb59e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1989a33f76eec4aa0aa316a425336d28",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "059690a3989aef666846",
+    "url": "/movie-app-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "79f9c712b8923d30afbb",
+    "url": "/movie-app-react/static/css/main.cf8c063a.chunk.css"
+  },
+  {
+    "revision": "059690a3989aef666846",
+    "url": "/movie-app-react/static/js/2.c662e5f3.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/movie-app-react/static/js/2.c662e5f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f9c712b8923d30afbb",
+    "url": "/movie-app-react/static/js/main.8a5c1474.chunk.js"
+  },
+  {
+    "revision": "5ebc74f1ddb738478335",
+    "url": "/movie-app-react/static/js/runtime-main.43c8e768.js"
+  }
+]);
